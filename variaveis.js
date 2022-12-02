ce8b609b8c4cbe7c9fa22a7fe501799c1e2ab302
Juanhen10 <91297117+Juanhen10@users.noreply.github.com>
2022-12-02 10:57:16 -0300

    Delete variaveis.js

--- variaveis.js
+++
@@ -1,7 +0,0 @@
-// variaveis define valores no programa
-let idade = 20;
-let nome = 'juan';
-console.log(idade)
-//usar o camelCase para nomear as variaveis
-let nomeCompleto = 'Juan Henrique'
-console.log(nomeCompleto)
